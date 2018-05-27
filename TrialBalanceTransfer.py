fa2f48cf4ce7691c946c2da3ecad102e40c73369
Chris E <cbeufemia@gmail.com>
2018-05-27 01:58:26 -0400

    fixed formatting

--- TrialBalanceTransfer.py
+++ TrialBalanceTransfer.py
@@ -3,127 +3,129 @@ from openpyxl.utils import column_index_from_string
 from openpyxl.utils import get_column_letter
 
 
-def SplitYears(Fullfile, Yearfile, start, end, initials, year):
+def split_years(full_file, year_file, start, end, initials, year):
 
-    Yearbook = openpyxl.load_workbook(Yearfile)
-    Fullbook = openpyxl.load_workbook(Fullfile)
-    Yearsheet = Yearbook.get_sheet_by_name(name='TB from Prior Mgmt')
-    Fullsheet = Fullbook.get_sheet_by_name(name='TB from Prior Mgmt')
-    Fullrange = range(column_index_from_string(start), column_index_from_string(end) + 3)
+    year_book = openpyxl.load_workbook(year_file)
+    full_book = openpyxl.load_workbook(full_file)
+    year_sheet = year_book.get_sheet_by_name(name='TB from Prior Mgmt')
+    full_sheet = full_book.get_sheet_by_name(name='TB from Prior Mgmt')
+    full_range = range(column_index_from_string(start), column_index_from_string(end) + 3)
 
     for index in range(1, 105):
         for j in range(4, 279):
-            Full = Fullrange[index - 1]
+            full = full_range[index - 1]
 
-            Yearsheet[get_column_letter(index) + str(j - 2)].value = Fullsheet[get_column_letter(Full) + str(j)].value
+            year_sheet[get_column_letter(index) + str(j - 2)].value = full_sheet[get_column_letter(full) + str(j)].value
 
-    Yearbook.save(
-        'C:\Users\T530\Desktop\Banta\\' + initials + '\\' + initials + year + ' Banta Management Chart of Account Mapping .xlsx')
+    year_book.save(
+        'C:\Users\T530\Desktop\Banta\\' + initials + '\\' + initials + year +
+        ' Banta Management Chart of Account Mapping .xlsx')
 
 
-def MoveTrialBalances(initials, length):
-    Target = openpyxl.load_workbook('C:\Users\T530\Desktop\Banta\Banta Management Chart of Account Mapping.xlsx')
-    TargetSheet = Target.get_sheet_by_name(name='TB from Prior Mgmt')
+def move_trial_balances(initials, length):
+    target = openpyxl.load_workbook('C:\Users\T530\Desktop\Banta\Banta Management Chart of Account Mapping.xlsx')
+    target_sheet = target.get_sheet_by_name(name='TB from Prior Mgmt')
 
-    Source = openpyxl.load_workbook('C:\Users\T530\Desktop\Banta\\' + initials + '\\' + initials + 'TB201312.xlsx')
-    SourceSheet = Source.get_sheet_by_name(name='Sheet1')
+    source = openpyxl.load_workbook('C:\Users\T530\Desktop\Banta\\' + initials + '\\' + initials + 'TB201312.xlsx')
+    source_sheet = source.get_sheet_by_name(name='Sheet1')
 
-    for i in range(14,length,1):
+    for i in range(14, length, 1):
         j = i-9
-        AccountNumColumn = get_column_letter(1)
-        AccountNameColumn = get_column_letter(2)
-        DebitColumn = get_column_letter(4)
-        CreditColumn = get_column_letter(5)
-        EndingColumn = get_column_letter(7)
+        account_num_column = get_column_letter(1)
+        account_name_column = get_column_letter(2)
+        debit_column = get_column_letter(4)
+        credit_column = get_column_letter(5)
+        ending_column = get_column_letter(7)
 
-        TargetSheet[AccountNumColumn + str(j)].value = SourceSheet['A' + str(i)].value
-        TargetSheet[AccountNameColumn + str(j)].value = SourceSheet['E' + str(i)].value
-        TargetSheet[DebitColumn + str(j)].value = SourceSheet['H' + str(i)].value
-        TargetSheet[CreditColumn + str(j)].value = SourceSheet['I' + str(i)].value
-        if TargetSheet[DebitColumn + str(j)].value or TargetSheet[DebitColumn + str(j)].value == 0:
-            TargetSheet[EndingColumn + str(j)].value = TargetSheet[DebitColumn + str(j)].value
-        elif TargetSheet[CreditColumn + str(j)].value or TargetSheet[CreditColumn + str(j)].value == 0:
-            TargetSheet[EndingColumn + str(j)].value = TargetSheet[CreditColumn + str(j)].value * -1
+        target_sheet[account_num_column + str(j)].value = source_sheet['A' + str(i)].value
+        target_sheet[account_name_column + str(j)].value = source_sheet['E' + str(i)].value
+        target_sheet[debit_column + str(j)].value = source_sheet['H' + str(i)].value
+        target_sheet[credit_column + str(j)].value = source_sheet['I' + str(i)].value
+        if target_sheet[debit_column + str(j)].value or target_sheet[debit_column + str(j)].value == 0:
+            target_sheet[ending_column + str(j)].value = target_sheet[debit_column + str(j)].value
+        elif target_sheet[credit_column + str(j)].value or target_sheet[credit_column + str(j)].value == 0:
+            target_sheet[ending_column + str(j)].value = target_sheet[credit_column + str(j)].value * -1
 
     for index in range(0, column_index_from_string('OR') / 8, 1):
 
-        AccountNumColumn = get_column_letter(index * 8 + 9)
-        AccountNameColumn = get_column_letter(index * 8 + 10)
-        OpeningColumn = get_column_letter(index * 8 + 11)
-        DebitColumn = get_column_letter(index * 8 + 12)
-        CreditColumn = get_column_letter(index * 8 + 13)
-        NetChangeColumn = get_column_letter(index * 8 + 14)
-        EndingColumn = get_column_letter(index * 8 + 15)
+        account_num_column = get_column_letter(index * 8 + 9)
+        account_name_column = get_column_letter(index * 8 + 10)
+        opening_column = get_column_letter(index * 8 + 11)
+        debit_column = get_column_letter(index * 8 + 12)
+        credit_column = get_column_letter(index * 8 + 13)
+        net_change_column = get_column_letter(index * 8 + 14)
+        ending_column = get_column_letter(index * 8 + 15)
 
-        year = 2014+ (index/12)
-        month = (index%12)+1
+        year = 2014 + (index/12)
+        month = (index % 12) + 1
         print str(month) + str(year)
 
-
-        '''if  month == 0:
-            try:
-                Source = openpyxl.load_workbook('C:\Users\T530\Desktop\Banta\\' + initials + '\\' + initials + 'TB' + str(year) + '12.xlsx')
-            except:
-                print "ERROR: " + str(month)+str(year)'''
-
         if month < 10:
-            Source = openpyxl.load_workbook('C:\Users\T530\Desktop\Banta\\'+initials+'\\'+initials+'TB' + str(year) + '0' + str(month) + '.xlsx')
+            source = openpyxl.load_workbook('C:\Users\T530\Desktop\Banta\\'+initials+'\\'+initials+'TB' + str(year) +
+                                            '0' + str(month) + '.xlsx')
 
         else:
-            Source = openpyxl.load_workbook('C:\Users\T530\Desktop\Banta\\'+initials+'\\'+initials+'TB' +str(year) + str(month) + '.xlsx')
+            source = openpyxl.load_workbook('C:\Users\T530\Desktop\Banta\\' + initials + '\\'+initials+'TB' + str(year)
+                                            + str(month) + '.xlsx')
 
-        SourceSheet = Source.get_sheet_by_name(name='Sheet1')
+        source_sheet = source.get_sheet_by_name(name='Sheet1')
 
         for i in range(14, length, 1):
             j = i - 9
-            TargetSheet[AccountNumColumn + str(j)].value = SourceSheet['A' + str(i)].value
-            TargetSheet[AccountNameColumn + str(j)].value = SourceSheet['E' + str(i)].value
+            target_sheet[account_num_column + str(j)].value = source_sheet['A' + str(i)].value
+            target_sheet[account_name_column + str(j)].value = source_sheet['E' + str(i)].value
 
-            if int(TargetSheet[AccountNumColumn + str(j)].value)  == 1996:
-                RetainedRow = j
+            if int(target_sheet[account_num_column + str(j)].value) == 1996:
+                retained_row = j
 
-            TargetSheet[OpeningColumn + str(j)].value = TargetSheet[get_column_letter(index * 8 + 7) + str(j)].value
+            target_sheet[opening_column + str(j)].value = target_sheet[get_column_letter(index * 8 + 7) + str(j)].value
 
+            if index % 12 == 0 and int(target_sheet[account_num_column + str(j)].value) > 1999:
+                target_sheet[opening_column + str(retained_row)].value += target_sheet[get_column_letter(index * 8 + 7)
+                                                                                       + str(j)].value
+                target_sheet[opening_column + str(j)].value = 0
 
-            if index % 12 == 0 and int(TargetSheet[AccountNumColumn + str(j)].value) > 1999:
-                TargetSheet[OpeningColumn + str(RetainedRow)].value += TargetSheet[get_column_letter(index * 8 + 7) + str(j)].value
-                TargetSheet[OpeningColumn + str(j)].value = 0
+            target_sheet[debit_column + str(j)].value = source_sheet['H' + str(i)].value
 
-            TargetSheet[DebitColumn + str(j)].value = SourceSheet['H' + str(i)].value
-
-            TargetSheet[CreditColumn + str(j)].value = SourceSheet['I' + str(i)].value
+            target_sheet[credit_column + str(j)].value = source_sheet['I' + str(i)].value
 
             if index < 1:
-                if TargetSheet['D' + str(i)].value:
-                    TargetSheet[EndingColumn + str(j)].value = TargetSheet['D' + str(i)].value
-                elif TargetSheet['E' + str(i)].value:
-                    TargetSheet[EndingColumn + str(j)].value = TargetSheet['E' + str(i)].value * -1
+                if target_sheet['D' + str(i)].value:
+                    target_sheet[ending_column + str(j)].value = target_sheet['D' + str(i)].value
+                elif target_sheet['E' + str(i)].value:
+                    target_sheet[ending_column + str(j)].value = target_sheet['E' + str(i)].value * -1
 
-            if TargetSheet[DebitColumn + str(j)].value or TargetSheet[DebitColumn + str(j)].value == 0:
-                TargetSheet[EndingColumn + str(j)].value = TargetSheet[DebitColumn + str(j)].value
-            elif TargetSheet[CreditColumn + str(j)].value or TargetSheet[CreditColumn + str(j)].value == 0:
-                TargetSheet[EndingColumn + str(j)].value = TargetSheet[CreditColumn + str(j)].value * -1
+            if target_sheet[debit_column + str(j)].value or target_sheet[debit_column + str(j)].value == 0:
+                target_sheet[ending_column + str(j)].value = target_sheet[debit_column + str(j)].value
+            elif target_sheet[credit_column + str(j)].value or target_sheet[credit_column + str(j)].value == 0:
+                target_sheet[ending_column + str(j)].value = target_sheet[credit_column + str(j)].value * -1
             else:
                 break
 
-            TargetSheet[NetChangeColumn + str(j)].value = TargetSheet[EndingColumn + str(j)].value - TargetSheet[OpeningColumn + str(j)].value
+            target_sheet[net_change_column + str(j)].value = target_sheet[ending_column + str(j)].value - target_sheet[
+                opening_column + str(j)].value
 
-    Target.save('C:\Users\T530\Desktop\Banta\Banta Management Chart of Account Mapping - '+initials+'.xlsx')
+    target.save('C:\Users\T530\Desktop\Banta\Banta Management Chart of Account Mapping - '+initials+'.xlsx')
 
-    SplitYears('C:\Users\T530\Desktop\Banta\Banta Management Chart of Account Mapping - '+initials+'.xlsx',
-         'C:\Users\T530\Desktop\Banta\Banta Management Chart of Account Mapping 2014.xlsx', 'A', 'CZ', initials, '2014')
-    SplitYears('C:\Users\T530\Desktop\Banta\Banta Management Chart of Account Mapping - '+initials+'.xlsx',
-         'C:\Users\T530\Desktop\Banta\Banta Management Chart of Account Mapping 2015.xlsx', 'CR', 'GS', initials, '2015')
-    SplitYears('C:\Users\T530\Desktop\Banta\Banta Management Chart of Account Mapping - '+initials+'.xlsx',
-         'C:\Users\T530\Desktop\Banta\Banta Management Chart of Account Mapping 2016.xlsx', 'GK', 'KK', initials, '2016')
-    SplitYears('C:\Users\T530\Desktop\Banta\Banta Management Chart of Account Mapping - '+initials+'.xlsx',
-         'C:\Users\T530\Desktop\Banta\Banta Management Chart of Account Mapping 2017.xlsx', 'KC', 'OC', initials, '2017')
-    SplitYears('C:\Users\T530\Desktop\Banta\Banta Management Chart of Account Mapping - '+initials+'.xlsx',
-         'C:\Users\T530\Desktop\Banta\Banta Management Chart of Account Mapping 2018.xlsx', 'NU', 'RS', initials, '2018')
+    split_years('C:\Users\T530\Desktop\Banta\Banta Management Chart of Account Mapping - '+initials+'.xlsx',
+                'C:\Users\T530\Desktop\Banta\Banta Management Chart of Account Mapping 2014.xlsx',
+                'A', 'CZ', initials, '2014')
+    split_years('C:\Users\T530\Desktop\Banta\Banta Management Chart of Account Mapping - '+initials+'.xlsx',
+                'C:\Users\T530\Desktop\Banta\Banta Management Chart of Account Mapping 2015.xlsx',
+                'CR', 'GS', initials, '2015')
+    split_years('C:\Users\T530\Desktop\Banta\Banta Management Chart of Account Mapping - '+initials+'.xlsx',
+                'C:\Users\T530\Desktop\Banta\Banta Management Chart of Account Mapping 2016.xlsx',
+                'GK', 'KK', initials, '2016')
+    split_years('C:\Users\T530\Desktop\Banta\Banta Management Chart of Account Mapping - '+initials+'.xlsx',
+                'C:\Users\T530\Desktop\Banta\Banta Management Chart of Account Mapping 2017.xlsx',
+                'KC', 'OC', initials, '2017')
+    split_years('C:\Users\T530\Desktop\Banta\Banta Management Chart of Account Mapping - '+initials+'.xlsx',
+                'C:\Users\T530\Desktop\Banta\Banta Management Chart of Account Mapping 2018.xlsx',
+                'NU', 'RS', initials, '2018')
 
 
-Inputs = {"HI":162, "MB":307, "SI":274}
+Inputs = {"HI": 162, "MB": 307, "SI": 274}
 
 for initial in Inputs.keys():
     print initial
-    MoveTrialBalances(initial, Inputs[initial])
+    move_trial_balances(initial, Inputs[initial])
